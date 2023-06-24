e80767604de6a4445ddfcccb6ec40572c2d72976
malinjr07 <malinjr07@gmail.com>
2023-06-24 20:43:28 +0600

    details section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { FC } from 'react';
 
 const Hero: FC = () => {
   return (
-    <section className=''>
+    <section className='mt-10'>
       <div className='flex flex-col justify-start items-start pl-64 pt-56 font-bold'>
         <h2 className='text-[40px] '>Hi, I am</h2>
         <h1 className='text-6xl mt-14'>Mosaddik Ali Maruf</h1>
@@ -12,32 +13,35 @@ const Hero: FC = () => {
           developer
         </h3>
         <div className='flex justify-between items-center gap-8 mt-32'>
-          <button
-            type='button'
+          <a
+            href='mailto:malinjr07@gmail.com'
+            target='_blank'
             className='bg-[#c4c4c4] text-black links text-4xl px-3 py-2 '
           >
             <i className='fa-solid fa-at'></i>
-          </button>
-          <button
-            type='button'
+          </a>
+          <a
+            href='https://github.com/malinjr07'
+            target='_blank'
             className='bg-[#c4c4c4] text-black links text-4xl px-3 py-2 '
           >
             <i className='fa-brands fa-github'></i>
-          </button>
-          <button
-            type='button'
+          </a>
+          <a
+            href='https://www.linkedin.com/in/malinjr07/'
+            target='_blank'
             className='bg-[#c4c4c4] text-black links text-4xl px-3 py-2 '
           >
             <i className='fa-brands fa-linkedin'></i>
-          </button>
+          </a>
         </div>
       </div>
       <div className='main-hero absolute top-0 right-0 z-10 w-[1008px] h-[880px] bg-[#071420]'>
         <Image
-          src='/hero.jpg'
+          src='/hero.png'
           alt="Maruf's Photo"
           fill
-          className='object-contain '
+          className='object-contain mt-10 '
         />
       </div>
     </section>
