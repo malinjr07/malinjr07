05096c735c0dce7db42a3c1d8736b7dd9e542488
malinjr07 <malinjr07@gmail.com>
2025-07-14 20:20:54 +0600

    feat: git ignore modified

--- vite-app/src/main.tsx
+++ vite-app/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './main-component/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/themify-icons.css';
+import './css/animate.css';
+import './css/animate.min.css';
+import './sass/style.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Get the root element with type assertion
+const rootElement = document.getElementById('root');
+
+// Ensure the root element exists
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+
+// Create a root and render the app
+const root = createRoot(rootElement);
+root.render(<App />);
