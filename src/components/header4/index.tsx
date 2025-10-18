31772d237fc9f5d985b278cef83011874ae8c5df
malinjr07 <malinjr07@gmail.com>
2025-10-18 14:03:30 +0600

    feat: Logo updated

--- src/components/header4/index.tsx
+++ src/components/header4/index.tsx
@@ -1,6 +1,6 @@
+import LogoWhite from 'components/svgs/LogoWhite';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
-import Logo from "../../images/logo.svg";
 
 interface Header4Props {
   /** Additional class name for the menu button */
@@ -12,14 +12,14 @@ interface Header4Props {
 const Header4: React.FC<Header4Props> = (props) => {
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
   const [isSticky, setSticky] = useState<boolean>(false);
-  
+
   const toggleMenu = (): void => {
     setMenuOpen(!isMenuOpen);
   };
 
   useEffect(() => {
     const handleScroll = (): void => {
-      const navbar = document.getElementById("navbar");
+      const navbar = document.getElementById('navbar');
       if (!navbar) return;
 
       const sticky = navbar.offsetTop;
@@ -34,23 +34,92 @@ const Header4: React.FC<Header4Props> = (props) => {
 
   return (
     <div>
-      <div className={props.mClass || ""} onClick={toggleMenu}>
-        <i className={isMenuOpen ? "ti-close" : "ti-align-justify"} id="bar_close"></i>
+      <div className={props.mClass || ''} onClick={toggleMenu}>
+        <i
+          className={isMenuOpen ? 'ti-close' : 'ti-align-justify'}
+          id='bar_close'
+        ></i>
       </div>
-      <header className={`${props.hclass || ""} ${isSticky ? "sticky" : ""}`} id="navbar">
-        <Link to="/" className="logo">
-          <img src={Logo as string} alt="" />
+      <header
+        className={`${props.hclass || ''} ${isSticky ? 'sticky' : ''}`}
+        id='navbar'
+      >
+        <Link to='/' className='logo'>
+          <LogoWhite width='155' height='40' />
         </Link>
-        <div className="header-section">
-          <div className={`menu ${isMenuOpen ? "open-mobilemenu" : ""}`} id="menu_wrap">
+        <div className='header-section'>
+          <div
+            className={`menu ${isMenuOpen ? 'open-mobilemenu' : ''}`}
+            id='menu_wrap'
+          >
             <ul>
-              <li><Link activeClass="active" to="top" spy={true} smooth={true} duration={500}>Home</Link></li>
-              <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>About</Link></li>
-              <li><Link activeClass="active" to="Services" spy={true} smooth={true} duration={500}>Services</Link></li>
-              <li><Link activeClass="active" to="Portfolio" spy={true} smooth={true} duration={500}>Portfolio</Link></li>
-              <li><Link activeClass="active" to="testimonial" spy={true} smooth={true} duration={500}>Testimonial</Link></li>
-              <li><Link activeClass="active" to="blog" spy={true} smooth={true} duration={500}>Blog</Link></li>
-              <li><Link activeClass="active" to="Contact" spy={true} smooth={true} duration={500}>Contact Me</Link></li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='top'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='about'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Services'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Portfolio'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='testimonial'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Testimonial
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Contact'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Contact Me
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
@@ -60,3 +129,4 @@ const Header4: React.FC<Header4Props> = (props) => {
 };
 
 export default Header4;
+
